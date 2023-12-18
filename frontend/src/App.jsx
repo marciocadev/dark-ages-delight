c99b926bd6bb6faa17c65c301309f240c083c5d3
Marcio Cruz de Almeida <marciocadev@gmail.com>
2023-12-17 22:55:00 -0300

    basic frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <>
+      {/* KEYBOARD CONTROLS */}
+      {/* SOCKET MANAGER */}
+      {/* CANVAS */}
+      {/* PHYSICS */}
+      {/* MENU */}
+      <h1>Olha</h1>
+    </>
+  )
+}
