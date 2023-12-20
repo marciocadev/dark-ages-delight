5a82d5a619799bb2e2810c479f650de0439450e6
Marcio Cruz de Almeida <marciocadev@gmail.com>
2023-12-19 21:47:58 -0300

    adicionando o mapa temporário

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
+import { Canvas } from '@react-three/fiber'
+import { Experience } from './components/Experience'
+
 export default function App() {
   return (
     <>
       {/* KEYBOARD CONTROLS */}
       {/* SOCKET MANAGER */}
       {/* CANVAS */}
+      <Canvas camera={{
+        position: [0, 6, 20],
+        fov: 42
+      }}>
+        <Experience />
+      </Canvas>
       {/* PHYSICS */}
       {/* MENU */}
-      <h1>Olha</h1>
+
     </>
   )
 }
